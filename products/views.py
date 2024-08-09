377e05cddcc3edface691b1dd6e0b93de1b44145
Alexander Hebbes <aeh7@live.co.uk>
2024-08-09 22:21:37 +0000

    add href to all products dropdown and css for mobile header

--- products/views.py
+++ products/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
+    print("all_products view called")
     """A View to show all products and sorting and searching  """
 
     products = Product.objects.all()
-    print(f"Number of products: {products.count()}")
+    
 
     context = {
         'products': products,
     }
     
     return render(request, 'products/products.html', context)
+
+
+
+def product_detail(request, product_id):
+    """A View to show product deatil """
+
+    product = get_object_or_404(Product, pk=product_id)
+    
+
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_detail.html', context)
