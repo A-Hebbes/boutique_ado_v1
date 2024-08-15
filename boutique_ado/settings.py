de9cefcfbe5fa2d0e0484e989d895412a89de543
Alexander Hebbes <aeh7@live.co.uk>
2024-08-15 23:10:52 +0000

    add forms etc

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -81,6 +84,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
---
+++ checkout/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class OrderForm(forms.Form):
+    # You'll add form fields here later
+    pass
